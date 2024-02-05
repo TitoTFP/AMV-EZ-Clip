44b72aead8095ea17fcb2b914b9e09a4402f7654
Tito Fauzan Putra <titofp371@gmail.com>
2024-02-05 23:26:19 +0700

    Changed from opening new shell to not opening new shell

---
+++ split_to.py
@@ -0,0 +1,14 @@
+import scenedetect
+import os
+import pathlib
+
+def split_to(output_dir, vid, seperate_clips_status):
+    #arguments = sys.argv
+    #random_number = arguments[4]
+        
+    
+    
+    exit()
+    
+if __name__ == "__main__":
+    split_to()
--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-command = 'start cmd /c scenedetect -i "D:/Videos/test/Landmine with Sign.mp4" -o "D:/Videos/test" split-video'
-
-try:
-    subprocess.Popen(command, shell=True)
-except subprocess.CalledProcessError as e:
-    print(f"Command failed with return code {e.returncode}")
